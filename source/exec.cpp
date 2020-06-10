e168f5a5eaaa5b3c417f60d78ee536d19c706a7b
Jeremias Kilian <jeremias.michael.kilian@uni-weimar.de>
2020-06-11 00:42:51 +0200

    it gets better (except for segfaults)

--- source/exec.cpp
+++ source/exec.cpp
@@ -4,6 +4,21 @@
 
 int main(int argc, char* argv[])
  {
-
+   Tree test;
+   std::cout << test.isempty() << "\n";
+   test.add(8);
+   std::cout << test.isempty() << "\n";
+   test.add(4);
+   std::cout << test.maximum()->key << "\n";
+   std::cout << test.minimum()->key << "\n";
+   test.rm(test.maximum());
+   std::cout << test.maximum()->key << "\n";
+   std::cout << test.isempty() << "\n";
+   test.rm(test.maximum());
+   std::cout << test.maximum()->key << "\n";
+   std::cout << test.isempty() << "\n";
+   test.rm(test.maximum());
+   std::cout << test.maximum()->key << "\n";
+   std::cout << test.isempty() << "\n";
   return 0;
  }
--- source/tree.hpp
+++ source/tree.hpp
@@ -18,7 +18,11 @@ class Tree {
   bool isempty() {
     return ((root_ == nullptr) ? true: false);
   }
-
+/*
+currnode is set to the very top of the tree, then we check if the key is equal to the int we are searching.
+if it is smaller, we go to the left child node (if it exsists)
+if it is bigger, we go to the ricght child node (if it exsists)
+*/
   Node* search(int i) {
     if (isempty()) return nullptr;
     Node* currnode = root_;
@@ -34,7 +38,9 @@ class Tree {
     }
     return ((currnode->key == i) ? currnode: nullptr);
   }
-
+/*
+iterates down the right hand side of the graph to find the biggest key value
+*/
   Node* maximum() {
     if (isempty()) return nullptr;
     Node* currnode = root_;
@@ -43,7 +49,9 @@ class Tree {
     }
     return currnode;
   }
-
+/*
+iterates down the left hand side of the graph to find the smallest key value
+*/
   Node* minimum() {
   if (isempty()) return nullptr;
     Node* currnode = root_;
@@ -52,7 +60,10 @@ class Tree {
     }
     return currnode;
   }
-
+/*
+first tests if the node is the maximum (then there would be no successor)
+then we search the tree, beginning from the root, and search for the smallest key value that is bigger than the key from n
+*/
   Node* successor(Node* n) {
     if (n->key == maximum()->key) return nullptr;
     Node* currnode = root_;
@@ -66,7 +77,10 @@ class Tree {
       else return currnode->parent;
     }
   }
-
+/*
+first tests if the node is the minimum (then there would be no successor)
+then we search the tree, beginning from the root, and search for the biggest key value that is smaller than the key from n
+*/
   Node* predecessor(Node* n) {
     if (n->key == minimum()->key) return nullptr;
     Node* currnode = root_;
@@ -80,18 +94,24 @@ class Tree {
       else return currnode;
     }
   }
-
-  void add(Node* n){
+/*
+test if the tree is empty. If it is we set the root to the new node
+*/
+  void add(int i){
     if (isempty()) {
+      Node* n = new Node;
+      n->key = i;
       root_ = n;
       return;
     }
-    if (search(n->key) != nullptr) {
-      std::cout << "node with the value " << n->key << " already in tree\n";
+    if (search(i) != nullptr) {
+      std::cout << "node with the value " << i << " already in tree\n";
     }
     Node* currnode = root_;
+    Node* n = new Node;
+    n->key = i;
     while (true) {
-      if(currnode->key < n->key) {
+      if(currnode->key < i) {
         if (currnode->right == nullptr) {
           currnode->right = n;
           n->parent = currnode;
@@ -105,33 +125,58 @@ class Tree {
           return;
         }
       }
-      (currnode->key > n->key) ? currnode = currnode->left : currnode = currnode->right;
+      (currnode->key > i) ? currnode = currnode->left : currnode = currnode->right;
     }
   }
 
   void rm(Node* n){
-    Node* p = n->parent;
+    Node* p;
+    Node* c;
+    //Option1
+    if ((n->left == nullptr) && (n->right == nullptr) && (n->parent == nullptr)) {
+      delete(n);
+      root_ = nullptr;
+      return;
+    }
     if ((n->left == nullptr) && (n->right == nullptr)) {
+      p = n->parent;
       (p->key > n->key) ? p->left = nullptr: p->right = nullptr;
       delete(n);
       return;
     }
-
+    //Option2
+    if (((n->left == nullptr) ^ (n->right == nullptr)) && (n->parent == nullptr)) {
+      (n->right == nullptr) ? c = n->left : c = n->right;
+      c->parent = nullptr; 
+    }
     if ((n->left == nullptr) ^ (n->right == nullptr)) {
-      Node* c;
+      p = n->parent;
       (n->right == nullptr) ? c = n->left : c = n->right;
       c->parent = p;
       (p->key > n->key) ? p->left = c : p->right = c;
       delete(n);
       return;
     }
+    //Option3
+    if((n->right != nullptr) && (n->left != nullptr)) {
+      if(n->right->left == nullptr) {
+        (n->parent == nullptr) ? p = nullptr : p = n->parent;
+        c = n->right;
+        c->parent = p;
+        c->left = n->left;
+        if (p == nullptr) root_ = c;
+        delete(n);
+        return;
+      }
+    }
+    //Option4
 
   }
 
   void print();
 
   private:
-  Node* root_;
+  Node* root_ = nullptr;
 };
 
 #endif
