049fc3e8a33d28556695d24b2ac12763fc4299e4
Jeremias Kilian <jeremias.michael.kilian@uni-weimar.de>
2020-07-08 15:48:09 +0200

    added comments and filtered out edge cases

--- source/exec.cpp
+++ source/exec.cpp
@@ -7,23 +7,41 @@
 #include <cmath> 
 
 int main(int argc, char* argv[]) {
-  int seed, size, rangeh, rangev;
-  std::cout << "please enter a seed for the number generation\n";
+
+  int seed, size, rangeh, rangev; 
+
+  //setting all the values absed on user input
+  std::cout << "-Please enter a seed for the number generation\n>";
   std::cin >> seed;
-  std::cout << "please now set the desired size of the set\n";
+  std::cout << "-Please now specify the number of points\n>";
   std::cin >> size;
-  std::cout << "lastly please enter the horizontal and vertical range\n";
+  std::cout << "-Lastly please enter the horizontal and vertical range\n>";
   std::cin >> rangeh;
+  std::cout << ">";
   std::cin >> rangev;
-  std::vector<point> pts(size);
+
+  //checking for bad inputs
+  if (size <= 2) {
+    std::cout << "!Error: size must be greater than two\n";
+    return 1;
+  }
+  if ((rangeh < 1) || (rangev < 1)) {
+    std::cout << "!Error: range too small or negative\n";
+    return 1;
+  }
+
+  //generating the set based on the requirements
+  std::cout << "\n-Thank you\n-Generating the set\n\n";
+  std::vector<point> pts(abs(size));
   for (auto &i : pts) {
     std::srand(seed);
     i.x = rand() % (rangeh + 1);
     i.y = rand() % (rangev + 1);
     seed++;
   }
+  std::cout << "-Generating complete: the following points are in the set:\n\n";
   for (auto i : pts) {
-    std::cout << i.x << " " << i.y << "\n";
+    std::cout << "->" << i.x << " " << i.y << "\n";
   }
   return 0;
 }
